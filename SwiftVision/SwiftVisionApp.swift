033c69f50b12bd8f8e051912a6cfb2731893d359
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-07-29 10:24:48 -0700

    Initial Commit

---
+++ SwiftVision/SwiftVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVisionApp.swift
+//  SwiftVision
+//
+//  Created by Alphonso Sensley II on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
